bc23cf8e2639bd0268486c777e0e510936dcea15
Vladislav Kuntsov <vladislav.kuntsov@gmail.com>
2021-07-18 16:29:25 +0300

    commit

---
+++ src/App/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './app.scss';
+import  logo from '../images/logo-plane.svg'
+
+function App() {
+
+  return (
+    <div className="root">
+      <div className='logo'>
+        <img className='logo-img' src={logo} alt='logo Aviasale'></img>
+      </div>
+    </div>
+  );
+}
+
+export default App;
